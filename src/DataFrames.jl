8d7aefd745273dcbff568de2391d5c734ac125f5
Alexey Stukalov <astukalov@gmail.com>
2015-08-24 23:41:18 +0200

    add helper functions for grouping and joining

--- src/DataFrames.jl
+++ src/DataFrames.jl
@@ -101,6 +101,7 @@ for (dir, filename) in [
         ("subdataframe", "subdataframe.jl"),
         ("groupeddataframe", "grouping.jl"),
         ("dataframerow", "dataframerow.jl"),
+        ("dataframerow", "utils.jl"),
 
         ("abstractdataframe", "iteration.jl"),
         ("abstractdataframe", "join.jl"),
